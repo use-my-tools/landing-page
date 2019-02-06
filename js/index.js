0bd7cefb43763e1b09814a75f782b880368ebe7d
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-02-06 15:58:22 -0800

    Added scrollTo. Fixed typos

--- js/index.js
+++ js/index.js
@@ -1,26 +1,9 @@
-// const container = document.querySelector('.container');
-
-// console.log(container);
 
 const getApi = document.querySelector('#get-api');
-
-//getApi.innerHTML = 
-// const getData = fetch('https://tools-backend.herokuapp.com/api/tools').then(res => res.json()).then(data => data);
-
-// console.log(getData);
-
-// function gettingData(){
-//     getApi.innerHTML = getData.data.map( tool => {
-//         console.log(tool)})
-// };
-
-// gettingData();
-
 
 // fetch('https://tools-backend.herokuapp.com/api/tools')
 //     .then(res => res.json())
 //     .then(data => data.data.map(tool => (tool)));
-
 
 fetch('https://tools-backend.herokuapp.com/api/tools')
 .then(res => res.json())
@@ -28,20 +11,13 @@ fetch('https://tools-backend.herokuapp.com/api/tools')
     getApi.innerHTML = data.data.length
 }))
 
-// let myTools = [];
-
-// fetch('https://tools-backend.herokuapp.com/api/tools')
-// .then(res => res.json())
-// .then(data => data.data((tool) => {
-//     console.log(tool.length);
-// }))
-
-
-// let myData;
-
-// fetch('https://tools-backend.herokuapp.com/api/tools')
-//     .then(res => res.json())
-//     .then(data => myData = data.data)
-
-
-// myData && console.log(myData);
+scrollTo = (element) => {
+    window.scrollTo({
+      behavior: 'smooth',
+      block: beginning
+    });
+}
+  
+document.getElementById("features").addEventListener('click', () => {
+    scrollTo(document.getElementById("features"));
+});
